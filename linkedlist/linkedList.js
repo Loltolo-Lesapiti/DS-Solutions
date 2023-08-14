c340d265ecc8eab224f2996d6e9315552acb9ea1
petro-lesapiti <petrolesapiti@gmail.com>
2023-08-14 15:45:04 +0300

    LinkedList operations

---
+++ linkedlist/linkedList.js
@@ -0,0 +1,67 @@
+/**
+ * Implementing LinkedList operations in JavaScript.
+ * The operations include.
+ * Add, size, delete, getLast, getLast, Clear. 
+ */
+    //Creating the node class.
+    class Node{
+        constructor(data){
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+    class LinkedList{
+        constructor(){
+            this.head=null;
+            this.tail=null;
+        }
+        // Add a node to the end of the list.
+        addToEnd=(data)=>{
+            let newNode= new Node(data);
+            if(!this.head){
+                this.head=newNode;
+                this.tail= newNode
+            }else{
+                this.tail.next=newNode;
+                this.tail=newNode;
+            }
+        }
+        //Add at the begining
+        addToBegin=(data)=>{
+            let newNode=new Node(data);
+            if(!this.head){
+                this.head=newNode;
+                this.tail=newNode;
+            }else{
+              newNode.next=this.head;
+              this.head=newNode;
+              this.tail.next=null;
+            }
+        }
+        //Print elements of an array
+        printAll=()=>{
+            let cur=this.head;
+            while(cur){
+                console.log(cur.data);
+                cur=cur.next;
+            }
+        }
+        // Checking the size of the linkedList.
+        size=()=>{
+            let count=0;
+            let node= this.head;
+            while(node){
+                count ++;
+                node=node.next;
+            }
+            return count;
+        }
+    }
+    
+    let list= new LinkedList();
+    list.addToBegin(10)
+    list.addToBegin(9)
+    list.addToBegin(11)
+
+    list.printAll();

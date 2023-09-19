e4d7336c2a0db3ce08f5f3d42453cf509ccd2700
Petro Lesapiti <petrolesapiti@gmail.com>
2023-09-19 13:40:47 +0300

    Improve missing num solution.

--- TuringPrep/missingNum.js
+++ TuringPrep/missingNum.js
@@ -1,12 +1,18 @@
 var missingNumber = function (nums) {
-  let numsSet = new Set(nums);
-  let missing = 0;
-  for (let i = 1; i <= nums.length; i++) {
-    if (!numsSet.has(i)) {
-      missing = i;
-      break;
-    }
-  }
-  return missing;
+  //   let numsSet = new Set(nums);
+  //   let missing = 0;
+  //   for (let i = 1; i <= nums.length; i++) {
+  //     if (!numsSet.has(i)) {
+  //       missing = i;
+  //       break;
+  //     }
+  //   }
+  // return missing;
+  //Better implementation that uses Gausian sum and the reduce function.
+  let expectedSum = nums.length(nums.length + 1) / 2;
+  let actualSum = nums.reduce((sum, num) => {
+    return sum + num;
+  }, 0);
+  return expectedSum - actualSum;
 };
 console.log(missingNumber([3, 0, 1]));

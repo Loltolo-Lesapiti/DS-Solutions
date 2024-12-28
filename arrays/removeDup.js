5333106f07f5f4560fc649f6efb33c014483c6b4
Petro Lesapiti <petrolesapiti@gmail.com>
2024-12-28 15:06:54 +0300

    Remove duplicate 1 leetcode.

--- arrays/removeDup.js
+++ arrays/removeDup.js
@@ -1,19 +1,13 @@
+const removeDuplicates = (nums) => {
+  const newSet = new Set(nums);
 
-const removeDuplicates = (nums)=> {
-    const newSet= new Set(nums);
-    
-    const newArray= [...newSet];
+  const newArray = [...newSet];
 
-    for(let i=0; i<newArray.length; i++){
-        nums[i]= newArray[i];
-    }
+  for (let i = 0; i < newArray.length; i++) {
+    nums[i] = newArray[i];
+  }
 
-    for(let i=newArray.length; i<nums.length; i++){
-        nums[i]="_"
-    }
-    
-    return nums;
-    
+  return nums;
 };
-let nums=[1,1,2];
+let nums = [1, 1, 2];
 console.log(removeDuplicates(nums));

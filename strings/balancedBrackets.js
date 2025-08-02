ad9f843793d8232a5f960ad5605d4d48fdf1f836
Petro Lesapiti <petrolesapiti@gmail.com>
2025-08-02 23:46:18 +0300

    Balanced brackets challenge.

--- strings/balancedBrackets.js
+++ strings/balancedBrackets.js
@@ -1,19 +1,31 @@
-
-const balanceBrackets=(expr)=>
-{
-    const stack=[];
-    for(let par of expr){
-        if(par==="("||par==="{"||par==="["){
-            stack.push(par);
-            continue
-        }
-        if (stack.length==0){
-            return false;
-        }
-        let check;
+const balance_brackets = (bracket) => {
+  //Step 1: Declaring variables
+  let stack = [];
+  let p1 = -1;
+  let p2 = 0;
+  //Step 2: Push the opeing parenthisis into the stack
+  for (par of bracket) {
+    if (par === "(" || par === "{" || par === "[") {
+      stack.push(par);
+      p2 += 1;
+      p1 += 1;
+      continue;
     }
-     
-    return stack.length==0;
-}
+    //Step 3: Check if the parenthese match.
+    if (
+      (stack[p1] === "(" && bracket[p2] === ")") ||
+      (stack[p1] === "[" && bracket[p2] === "]") ||
+      (stack[p1] === "{" && bracket[p2] === "}")
+    ) {
+      stack.pop();
+      p1 -= 1;
+      p2 += 1;
+    } else {
+      break;
+    }
+  }
+  //Step: Check if the stack is empty and return a boolean.
+  return stack.length == 0;
+};
 
-console.log(balanceBrackets("({[]}}]"));
+console.log(balance_brackets("({[{()}]})"));

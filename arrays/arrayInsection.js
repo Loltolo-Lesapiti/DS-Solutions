6d85e7870ae5b365a6555ecfa9d4d81ce9636960
Petro Lesapiti <petrolesapiti@gmail.com>
2025-08-27 03:17:56 +0300

    Review  array intersection solution.

--- arrays/arrayInsection.js
+++ arrays/arrayInsection.js
@@ -1,15 +1,16 @@
-const interSection=(arr1, arr2)=>{
-    //Create the sets out of the arrays to avoid duplicates.
-    const arr1Set=new Set(arr1);
-    const arr2Set=new Set(arr2);
-    const common=[];
-    
-    for(elem of arr1Set){
-        if(arr2Set.has(elem)){
-            common.push(elem);
-        }
+const interSection = (arr1, arr2) => {
+  //Create the sets out of the arrays to avoid duplicates.
+  const arr1Set = new Set(arr1);
+  const arr2Set = new Set(arr2);
+  //An array to store intersection
+  const common = [];
+
+  for (elem of arr1Set) {
+    if (arr2Set.has(elem)) {
+      common.push(elem);
     }
-    return common;
-    }
-    
-    console.log(interSection([1, 2, 3, 4, 5],[2, 3, 5, 6, 7]))
+  }
+  return common;
+};
+
+console.log(interSection([1, 2, 3, 4, 5], [2, 3, 5, 6, 7]));
